95d430c8631ad0bc9c5070a65bbafc3f7a797af6
Mahiler <fernando.mahiler@gmail.com>
2021-01-25 15:36:53 -0500

    Initial Commit

---
+++ src/JournalApp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import paisaje from './assets/images/17010.jpg';
+
+const JournalApp = () => {
+    console.log('');
+    return (
+        <div>
+            <img src={paisaje} alt='paisaje' />
+            <h1>Soy un nuevo componente</h1>
+        </div>
+    );
+};
+
+export default JournalApp;
